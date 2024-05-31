7287adcd80d9fde8d16be064d277577eb51239f5
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-05-31 16:50:24 +0530

    add toggle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
