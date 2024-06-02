c45d1bbbe05fdf1db4ffa741375aa5eac805aa5f
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-06-02 22:02:40 +0530

    add sidenav

--- src/app/admin/admin-layout/admin-layout.component.ts
+++ src/app/admin/admin-layout/admin-layout.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { AuthService } from "../services/auth.service";
 import { RouterModule, RouterOutlet } from "@angular/router";
+import { MatSidenavModule } from "@angular/material/sidenav";
 
 @Component({
   selector: "app-admin-layout",
   standalone: true,
-  imports: [RouterOutlet, RouterModule],
+  imports: [RouterOutlet, RouterModule, MatSidenavModule],
   templateUrl: "./admin-layout.component.html",
   styleUrl: "./admin-layout.component.scss"
 })
