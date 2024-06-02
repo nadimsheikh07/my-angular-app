c055a1d1d919e330e5e6c49ed88b35187b1cd365
Nadim Sheikh <nadim.sheikh.07@gmail.com>
2024-06-02 21:44:19 +0530

    add users component

--- src/app/admin/admin-routing.module.ts
+++ src/app/admin/admin-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { authGuard } from "./guards/auth.guard";
+import { UsersComponent } from "./users/users.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: "", redirectTo: "dashboard", pathMatch: "full" },
   {
     path: "dashboard",
-    component: DashboardComponent,
-    canActivate: [authGuard],    
+    component: DashboardComponent
+  },
+  {
+    path: "users",
+    component: UsersComponent
   }
 ];
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,14 @@ import { Routes } from "@angular/router";
 import { AdminLayoutComponent } from "./admin/admin-layout/admin-layout.component";
 import { WebsiteLayoutComponent } from "./website/website-layout/website-layout.component";
 import { LoginComponent } from "./admin/login/login.component";
+import { authGuard } from "./admin/guards/auth.guard";
 
 export const routes: Routes = [
   {
     path: "admin",
     component: AdminLayoutComponent,
-    loadChildren: () => import("./admin/admin.module").then(m => m.AdminModule)
+    loadChildren: () => import("./admin/admin.module").then(m => m.AdminModule),
+    canActivate: [authGuard]
   },
   { path: "admin/login", component: LoginComponent },
   {
